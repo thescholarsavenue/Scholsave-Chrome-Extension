f756be28ecadfbd7afb18b6bfd96d71f6a5af7ce
sriramsridharan001 <sriramsridharan001@gmail.com>
2013-08-19 12:20:09 +0530

    Second Commit
    @Ajay, Pranav: I had to make some drastic changes so as to implement
    push notifications. I haven't implemented push notifications yet. The
    current code does the same thing which the older app did.  While I'm
    working on adding PNs, can you guys check if this thing has any bugs?

--- scripts/background.js
+++ scripts/background.js
@@ -1,39 +1,188 @@
-chrome.extension.onRequest.addListener(function(request, sender, callback){
-	if (request.action == 'fetch_home_feed')
-		fetch_home_feed(request.url);
-	if (request.action == 'fetch_notice_feed')
-		fetch_notice_feed(request.url);
-	if (request.action == 'fetch_blog_feed')
-		fetch_fb_feed(request.url);
-});
+var timeoutID = window.setTimeout(fetch_home_feed, 10000);
+var timeoutID = window.setTimeout(fetch_notice_feed, 10000);
+var timeoutID = window.setTimeout(fetch_blog_feed, 10000);
+var timeoutID = window.setTimeout(clear_data, 9000);
 
-
-function fetch_home_feed(url) {
+function fetch_home_feed() {
 	var result = new XMLHttpRequest();
-	result.open('GET', url, true);
+	result.open('GET', 'http://www.scholarsavenue.org/feed/', true);
 	result.send();    
 	result.onreadystatechange = function(data) {
 		if (result.readyState == 4 && result.status == 200)
 			localStorage.setItem('feed_home_call', result.responseText);
+		else {
+			localStorage.setItem('feed_home_call', null);
+		}
 	}
+	parse_home();
 }
 
-function fetch_notice_feed(url) {
+function fetch_notice_feed() {
 	var result = new XMLHttpRequest();
-	result.open('GET', url, true);
+	result.open('GET', 'http://noticeboard.scholarsavenue.org/feed/', true);
 	result.send();    
 	result.onreadystatechange = function(data) {
 		if (result.readyState == 4 && result.status == 200)
 			localStorage.setItem('feed_notice_call', result.responseText);
+		else {
+			localStorage.setItem('feed_notice_call', null);
+		}	
 	}
+	parse_notice();
 }
 
-function fetch_fb_feed(url) {
+function fetch_blog_feed() {
 	var result = new XMLHttpRequest();
-	result.open('GET', url, true);
+	result.open('GET', 'http://blog.scholarsavenue.org/feed/', true);
 	result.send();    
 	result.onreadystatechange = function(data) {
 		if (result.readyState == 4 && result.status == 200)
 			localStorage.setItem('feed_blog_call', result.responseText);
+		else {
+			localStorage.setItem('feed_blog_call', null);
+		}	
 	}
+	parse_blog();
+}
+
+function parse_home(){
+	if(localStorage.getItem('feed_home_call')==null) {
+		var item = '';
+		item += '<div class="error">\
+				<p>Oops!</p>\
+				<p>There seems to be a problem with your internet connection.</p>\
+				<p>Please try again later.</p>\
+				</div>';
+		localStorage.setItem('feed_home_data', item);
+	}
+	else {
+		var xml_doc = $($.parseXML(localStorage.getItem('feed_home_call')));
+		var i=1;
+		var box = 'box';
+		xml_doc.find("item").each(function(index, element) {
+			box = box + i;
+			i++;
+			if(i>11)
+				exit();
+			var post= new Object();
+			post.title = $(element).find("title").text();
+			post.url = $(element).find("link").text();
+			post.img=$(unescape($(element).find("encoded").text())).find("img").attr("src");
+			if(post.img=="http://feeds.feedburner.com/~ff/TheScholarsAvenue?d=yIl2AUoC8zA" || post.img==undefined)
+				post.img="images/nothumb.jpg";
+			post.description = $(element).find("description").text();
+			post.description=$.trim(post.description);
+			post.description = post.description.substr(0, 120);
+			var item = '';
+			item += '<label><div class="' + box + '">';
+			item += '<div class="thumb">\
+			<img src="' + post.img + '" height="75px" width="75px"></img></div>';
+			item += '<div class="item">\
+			<h4>' + post.title + '</h4>\
+			<p class="description">' + post.description + '...</p>\
+			<p hidden class="link">' + post.url + '</p>\
+			</div></div></label>';
+			appendToStorage('feed_home_data',item);
+			box = 'box';
+		});
+	}
+}
+
+function parse_notice(){
+	if(localStorage.getItem('feed_notice_call')==null) {
+		var item = '';
+		item += '<div class="error">\
+				<p>Oops!</p>\
+				<p>There seems to be a problem with your internet connection.</p>\
+				<p>Please try again later.</p>\
+				</div>';
+		localStorage.setItem('feed_notice_data', item);
+	}
+	else {
+		var xml_doc = $($.parseXML(localStorage.getItem('feed_notice_call')));
+		var i=1;
+		var box = 'box';
+		xml_doc.find("item").each(function(index, element) {
+			box = box + i;
+			i++;
+			if(i>11)
+				exit();			
+			var post= new Object();
+			post.title = $(element).find("title").text();
+			post.url = $(element).find("link").text();
+			post.img=$(unescape($(element).find("encoded").text())).find("img").attr("src");
+			if(post.img==undefined)
+				post.img="images/nothumb.jpg";
+			post.description = $(element).find("description").text();
+			post.description=$.trim(post.description);
+			post.description = post.description.substr(0, 120);
+			var item = '';
+			item += '<label><div class="' + box + '">';
+			item += '<div class="thumb">\
+			<img src="' + post.img + '" height="75px" width="75px"></img></div>';
+			item += '<div class="item">\
+			<h4>' + post.title + '</h4>\
+			<p class="description">' + post.description + '...</p>\
+			<p hidden class="link">' + post.url + '</p>\
+			</div></div></label>';
+			appendToStorage('feed_notice_data',item);
+			box = 'box';
+		});
+	}
+}
+
+function parse_blog(){
+	if(localStorage.getItem('feed_blog_call')==null) {
+		var item = '';
+		item += '<div class="error">\
+				<p>Oops!</p>\
+				<p>There seems to be a problem with your internet connection.</p>\
+				<p>Please try again later.</p>\
+				</div>';
+		localStorage.setItem('feed_blog_data', item);
+	}
+	else {
+		var xml_doc = $($.parseXML(localStorage.getItem('feed_blog_call')));
+		var i=1;
+		var box = 'box';
+		xml_doc.find("item").each(function(index, element) {
+			box = box + i;
+			i++;
+			if(i>11)
+				exit();
+			var post= new Object();
+			post.title = $(element).find("title").text();
+			post.url = $(element).find("link").text();
+			post.img=$(unescape($(element).find("encoded").text())).find("img").attr("src");
+			if(post.img==undefined)
+				post.img="images/nothumb.jpg";
+			post.description = $(element).find("description").text();
+			post.description=$.trim(post.description);
+			post.description = post.description.substr(0, 120);
+			var item = '';
+			item += '<label><div class="' + box + '">';
+			item += '<div class="thumb">\
+			<img src="' + post.img + '" height="75px" width="75px"></img></div>';
+			item += '<div class="item">\
+			<h4>' + post.title + '</h4>\
+			<p class="description">' + post.description + '...</p>\
+			<p hidden class="link">' + post.url + '</p>\
+			</div></div></label>';
+			appendToStorage('feed_blog_data',item);
+			box = 'box';
+	});
+	}
+}
+
+function appendToStorage(name, data) {
+    var old = localStorage.getItem(name);
+    if(old === null) 
+		old = '';
+    localStorage.setItem(name, old + data);
+}
+
+function clear_data() {
+	localStorage.removeItem('feed_home_data');
+	localStorage.removeItem('feed_notice_data');
+	localStorage.removeItem('feed_blog_data');
 }
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,4 @@
 var i=1;
-var j=1;
 
 $(document).ready(function() {
 	$(function() {
@@ -35,127 +34,240 @@ $(document).ready(function() {
 
 function display_home(){
 	$('#tabs-1').empty();
-	chrome.extension.sendRequest({'action' : 'fetch_home_feed', 'url' : 'http://feeds.feedburner.com/TheScholarsAvenue/'}, function(response){});
-	if(localStorage.getItem('feed_home_call')==null) {
-		var item = '';
-		item += '<div class="error">\
-				<p>Oops!</p>\
-				<p>There seems to be a problem with your internet connection.</p>\
-				<p>Please try again later.</p>\
-				</div>';
-		$('#tabs-1').append(item);
-	}
-	else {
-		var xml_doc = $($.parseXML(localStorage.getItem('feed_home_call')));
-		xml_doc.find("item").each(function(index, element) {
-			var post= new Object();
-			post.title = $(element).find("title").text();
-			post.url = $(element).find("link").text();
-			post.img=$(unescape($(element).find("encoded").text())).find("img").attr("src");
-			if(post.img=="http://feeds.feedburner.com/~ff/TheScholarsAvenue?d=yIl2AUoC8zA")
-				post.img="images/nothumb.jpg";
-			if(post.img==undefined)
-				post.img="images/nothumb.jpg";
-			post.description = $(element).find("description").text();
-			post.description=$.trim(post.description);
-			post.description = post.description.substr(0, 120);
-			var item = '';
-			item += '<label><div class="box">';
-			item += '<div class="thumb">\
-			<img src="' + post.img + '" height="75px" width="75px"></img></div>';
-			item += '<div class="item">\
-			<h4>' + post.title + '</h4>\
-			<p class="description">' + post.description + '...</p>\
-			</div></div></label>';
-			$('#tabs-1').append(item);
-			openlink(post.url);
-		});
-	}
+	$('#tabs-1').append(localStorage.getItem('feed_home_data'));
+	openlink_home();
 }
 
 function display_notice(){
 	$('#tabs-2').empty();
-	chrome.extension.sendRequest({'action' : 'fetch_notice_feed', 'url' : 'http://noticeboard.scholarsavenue.org/feed/'}, function(response){});
-	if(localStorage.getItem('feed_notice_call')==null) {
-		var item = '';
-		item += '<div class="error">\
-				<p>Oops!</p>\
-				<p>There seems to be a problem with your internet connection.</p>\
-				<p>Please try again later.</p>\
-				</div>';
-		$('#tabs-2').append(item);
-	}
-	else {
-		var xml_doc = $($.parseXML(localStorage.getItem('feed_notice_call')));
-		xml_doc.find("item").each(function(index, element) {
-			var post= new Object();
-			post.title = $(element).find("title").text();
-			post.url = $(element).find("link").text();
-			post.img=$(unescape($(element).find("encoded").text())).find("img").attr("src");
-			if(post.img==undefined)
-				post.img="images/nothumb.jpg";
-			post.description = $(element).find("description").text();
-			post.description=$.trim(post.description);
-			post.description = post.description.substr(0, 120);
-			var item = '';
-			item += '<label><div class="box">';
-			item += '<div class="thumb">\
-			<img src="' + post.img + '" height="75px" width="75px"></img></div>';
-			item += '<div class="item">\
-			<h4>' + post.title + '</h4>\
-			<p class="description">' + post.description + '...</p>\
-			</div></div></label>';
-			$('#tabs-2').append(item);
-			openlink(post.url);
-		});
-	}
+	$('#tabs-2').append(localStorage.getItem('feed_notice_data'));
+	openlink_notice();
 }
 
 function display_blog(){
 	$('#tabs-3').empty();
-	chrome.extension.sendRequest({'action' : 'fetch_blog_feed', 'url' : 'http://blog.scholarsavenue.org/feed/'}, function(response){});
-	if(localStorage.getItem('feed_blog_call')==null) {
-		var item = '';
-		item += '<div class="error">\
-				<p>Oops!</p>\
-				<p>There seems to be a problem with your internet connection.</p>\
-				<p>Please try again later.</p>\
-				</div>';
-		$('#tabs-3').append(item);
-	}
-	else {
-		var xml_doc = $($.parseXML(localStorage.getItem('feed_blog_call')));
-		xml_doc.find("item").each(function(index, element) {
-			var post= new Object();
-			post.title = $(element).find("title").text();
-			post.url = $(element).find("link").text();
-			post.img=$(unescape($(element).find("encoded").text())).find("img").attr("src");
-			if(post.img==undefined)
-				post.img="images/nothumb.jpg";
-			post.description = $(element).find("description").text();
-			post.description=$.trim(post.description);
-			post.description = post.description.substr(0, 120);
-			var item = '';
-			item += '<label><div class="box">';
-			item += '<div class="thumb">\
-			<img src="' + post.img + '" height="75px" width="75px"></img></div>';
-			item += '<div class="item">\
-			<h4>' + post.title + '</h4>\
-			<p class="description">' + post.description + '...</p>\
-			</div></div></label>';
-			$('#tabs-3').append(item);
-			openlink(post.url);
-	});
-	}
+	$('#tabs-3').append(localStorage.getItem('feed_blog_data'));
+	openlink_blog();
 }
 
-function openlink(thelink){
-		$('.box').click(function(){
-			chrome.tabs.create({url: thelink, active: false});	
-			i++;
-			if(i!=1){
-				i=1;
-				exit();
-			}
+function openlink_home(){
+	var thelink;
+	$('.box1').click(function(){
+		$('#tabs-1').find(".link").each(function(index, element) {
+			if(index==0)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box2').click(function(){
+		$('#tabs-1').find(".link").each(function(index, element) {
+			if(index==1)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box3').click(function(){
+		$('#tabs-1').find(".link").each(function(index, element) {
+			if(index==2)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box4').click(function(){
+		$('#tabs-1').find(".link").each(function(index, element) {
+			if(index==3)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box5').click(function(){
+		$('#tabs-1').find(".link").each(function(index, element) {
+			if(index==4)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box6').click(function(){
+		$('#tabs-1').find(".link").each(function(index, element) {
+			if(index==5)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box7').click(function(){
+		$('#tabs-1').find(".link").each(function(index, element) {
+			if(index==6)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box8').click(function(){
+		$('#tabs-1').find(".link").each(function(index, element) {
+			if(index==7)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box9').click(function(){
+		$('#tabs-1').find(".link").each(function(index, element) {
+			if(index==8)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box10').click(function(){
+		$('#tabs-1').find(".link").each(function(index, element) {
+			if(index==9)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+}
+
+function openlink_notice(){
+	var thelink;
+	$('.box1').click(function(){
+		$('#tabs-2').find(".link").each(function(index, element) {
+			if(index==0)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box2').click(function(){
+		$('#tabs-2').find(".link").each(function(index, element) {
+			if(index==1)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box3').click(function(){
+		$('#tabs-2').find(".link").each(function(index, element) {
+			if(index==2)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box4').click(function(){
+		$('#tabs-2').find(".link").each(function(index, element) {
+			if(index==3)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box5').click(function(){
+		$('#tabs-2').find(".link").each(function(index, element) {
+			if(index==4)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box6').click(function(){
+		$('#tabs-2').find(".link").each(function(index, element) {
+			if(index==5)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box7').click(function(){
+		$('#tabs-2').find(".link").each(function(index, element) {
+			if(index==6)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box8').click(function(){
+		$('#tabs-2').find(".link").each(function(index, element) {
+			if(index==7)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box9').click(function(){
+		$('#tabs-2').find(".link").each(function(index, element) {
+			if(index==8)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box10').click(function(){
+		$('#tabs-2').find(".link").each(function(index, element) {
+			if(index==9)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+}
+
+function openlink_blog(){
+	var thelink;
+	$('.box1').click(function(){
+		$('#tabs-3').find(".link").each(function(index, element) {
+			if(index==0)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box2').click(function(){
+		$('#tabs-3').find(".link").each(function(index, element) {
+			if(index==1)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box3').click(function(){
+		$('#tabs-3').find(".link").each(function(index, element) {
+			if(index==2)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box4').click(function(){
+		$('#tabs-3').find(".link").each(function(index, element) {
+			if(index==3)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box5').click(function(){
+		$('#tabs-3').find(".link").each(function(index, element) {
+			if(index==4)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box6').click(function(){
+		$('#tabs-3').find(".link").each(function(index, element) {
+			if(index==5)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box7').click(function(){
+		$('#tabs-3').find(".link").each(function(index, element) {
+			if(index==6)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box8').click(function(){
+		$('#tabs-3').find(".link").each(function(index, element) {
+			if(index==7)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box9').click(function(){
+		$('#tabs-3').find(".link").each(function(index, element) {
+			if(index==8)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
+	});
+	$('.box10').click(function(){
+		$('#tabs-3').find(".link").each(function(index, element) {
+			if(index==9)
+				thelink=$(element).text();
+		});
+		chrome.tabs.create({url: thelink, active: false});
 	});
 }
