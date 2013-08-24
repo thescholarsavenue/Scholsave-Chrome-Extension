7ce9a1ecb2321b13e498684dcaee36a16f8bf2a9
sriramsridharan001 <sriramsridharan001@gmail.com>
2013-08-24 11:15:56 +0530

    Added Google Analytics

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,3 +1,13 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-43460535-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 var item = '';
 item += '<div class="error">\
 		<p>Oops!</p>\
